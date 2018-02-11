7528861773a7d4f00e8beaf039fea3cb822daa73
sagara911@gmail.com <Lachaumiere1!>
2018-02-11 19:23:52 +0100

    Tentative automatisation

--- request_cob.js
+++ request_cob.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var pairs = [];
 
 var host ="https://api.cobinhood.com"
 //Http to curl :https://curlbuilder.com/
@@ -16,19 +17,63 @@ var get_trading_statistics="/v1/market/stats/"
 var get_tickers           = "/v1/market/tickers/" // /v1/market/tickers/<trading_pair_id> get with get_all_trading_pairs
 var recent_trades         ="/v1/market/trades/"    // /v1/market/trades//<trading_pair_id> get with get_all_trading_pairs      
 var options = {
-    url: host+get_order_book+"COB-BTC"
+    //url: host+get_order_book+paire
 };
 
-function callback(error, response, body) {
+
+// options.url=host+get_trading_statistics
+// function callback(error, response, body) {
+    
+//     if (!error && response.statusCode == 200) {
+//         // console.log(json_response);
+//         var json_response =JSON.parse(body).result[0]
+
+//         // console.log(options.url);
+//         console.log(json_response)
+//     }
+// }
+
+options.url=host+get_all_trading_pairs
+function callback(error, response, body) {    
     if (!error && response.statusCode == 200) {
-        var json_response =JSON.parse(body).result.orderbook
-        var spread = ((json_response.bids[0][0]-json_response.asks[0][0])/json_response.bids[0][0]*100)
-        console.log(json_response.bids[0]);
-        console.log(json_response.asks[0]);
-        console.log(spread);
+        // console.log(json_response);
+        var json_response =JSON.parse(body).result.trading_pairs
+        for (var pair in json_response) {
+            pairs.push(json_response[pair].id)
+          }
+          
+
+ 
+for (var pair in pairs) {
+
+    console.log("paire :"+pairs[pair])
+
+    options.url=host+get_order_book+pairs[pair]
+    function callback(error, response, body) {
+        console.log(options.url)
+        if (!error && response.statusCode == 200) {
+            var json_response =JSON.parse(body).result.orderbook
+            var spread = ((json_response.bids[0][0]-json_response.asks[0][0])/json_response.bids[0][0]*100)
+            //console.log(json_response.bids[0]);
+            //console.log(json_response.asks[0]);
+            console.log(paire + " :"+spread);
+        }
+    }
+  }
+        // console.log(options.url);
+        
     }
 }
 
+// for (var pair in pairs) {
+//     console.log("paire :"+pairs[pair])
+//   }
 
+
+
+
+console.log(pairs)
 request(options, callback);
-console.log(options.url)
+
+
+
